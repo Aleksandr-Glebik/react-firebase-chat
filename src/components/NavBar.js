ca3e96bb59c4ed412a106abf4fa15c0b31f861e0
Aleksandr-Glebik <glebikaa@mail.ru>
2022-10-18 14:13:51 +0300

    feat: add comp NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Grid from '@mui/material/Grid'
+import { Button } from '@mui/material'
+import { Link } from 'react-router-dom'
+import { LOGIN_ROUTE } from '../utils/consts'
 
 const NavBar = () => {
+    const user = false
+
     return (
-        <div>
-            NavBar
-        </div>
+        <AppBar position="static" color={'primary'} textColor={'white'}>
+            <Toolbar>
+                <Grid container justifyContent={'flex-end'}>
+                    { user
+                        ? <Button variant={'outlined'} color="inherit">Выйти</Button>
+                        : <Link to={LOGIN_ROUTE} underline="none">
+                            <Button variant={'outlined'} color="inherit">Логин</Button>
+                          </Link>
+                    }
+                </Grid>
+            </Toolbar>
+        </AppBar>
     )
 }
 
